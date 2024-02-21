950b5fcfca99b0d45412713564bede1a96dfe531
kspencerl <kimberly.liz@sga.pucminas.br>
2024-02-21 09:15:46 -0300

    update 1.0

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#pip install -r requirements.txt
+
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
@@ -5,9 +7,9 @@ from flask_sqlalchemy import SQLAlchemy
 #instanciar Flask
 app = Flask(__name__)
 #adicionar database sqlite
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ekrypto.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ekrypto.db' #nome para o database
 #configurar password
-app.config['SECRET_KEY'] = '12345'
+app.config['SECRET_KEY'] = '12345' #senha para o database
 #inicializar banco de dados
 db = SQLAlchemy(app)
 
